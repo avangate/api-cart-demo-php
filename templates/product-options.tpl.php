46aeadb87e90159da812310f664409899ca5e6bf
Marius Orcsik <marius.orcsik@avangate.com>
2014-02-04 18:06:12 +0200

    Some semicolons

--- templates/product-options.tpl.php
+++ templates/product-options.tpl.php
@@ -2,13 +2,13 @@
 <?php /* @var $OptionGroup mPriceOptionGroup */ 
 foreach ($prod->PriceOptions as $iKey => $OptionGroup) { ?>
 				<dt class="opt_group1">
-					 <strong><?php echo $OptionGroup->Name ?><?php if ($OptionGroup->Required) { echo '<sup class="mandatory">*</sup>'; } ?></strong>
+					 <strong><?php echo $OptionGroup->Name; ?><?php if ($OptionGroup->Required) { echo '<sup class="mandatory">*</sup>'; } ?></strong>
 				</dt> 
 				<dd style="padding-left:10px">
 <?php
 	if ($OptionGroup->Type == 'COMBO') { 
 ?>
-					<select name="<?php echo $OptionGroup->Name ?>" <? echo ($OptionGroup->Required ? 'class="required"' : '') ?> > 
+					<select name="<?php echo $OptionGroup->Name; ?>" <? echo ($OptionGroup->Required ? 'class="required"' : '') ?> > 
 <?php 
 	if (!$OptionGroup->Required) { 
 ?> 
@@ -17,21 +17,21 @@ foreach ($prod->PriceOptions as $iKey => $OptionGroup) { ?>
 	}
 		/* @var $Option mPriceOptionOption */
 		foreach ($OptionGroup->Options as $iOptionKey => $Option) { ?>
-						<option value="<?php echo $Option->Value ?>" <?php echo $Option->Default ? "selected='selected'" : '' ?>><?php echo $Option->Name ?> </option>
+						<option value="<?php echo $Option->Value; ?>" <?php echo $Option->Default ? "selected='selected'" : '' ?>><?php echo $Option->Name; ?> </option>
 <?php		}  ?>
 					</select>
 		
 <?php	} elseif ($OptionGroup->Type == 'INTERVAL') { ?>
-					<label><?php echo $OptionGroup->Name ?> <input <? echo ($OptionGroup->Required ? 'class="required"' : '') ?> type="text" name="<?php echo $OptionGroup->Name ?>" /></label>
+					<label><?php echo $OptionGroup->Name; ?> <input <? echo ($OptionGroup->Required ? 'class="required"' : '') ?> type="text" name="<?php echo $OptionGroup->Name; ?>" /></label>
 <?php 
 	} else {
 		if (!$OptionGroup->Required && $OptionGroup->Type == 'RADIO') {
 ?>
-					<label>none <input type="<? echo strtolower($OptionGroup->Type) ?>" checked='checked' name="<?php echo $OptionGroup->Name ?>" value="" /></label>
+					<label>none <input type="<? echo strtolower($OptionGroup->Type) ?>" checked='checked' name="<?php echo $OptionGroup->Name; ?>" value="" /></label>
 <?php 
 		}
 		foreach ($OptionGroup->Options as $iOptionKey => $Option) { ?>
-					<label><?php echo $Option->Name ?> <input <? echo ($OptionGroup->Required ? 'class="required"' : '') ?> type="<? echo strtolower($OptionGroup->Type) ?>" <?php echo $Option->Default ? "checked='checked'" : '' ?> name="<?php echo $OptionGroup->Name ?><?php echo ($OptionGroup->Type=='CHECKBOX' ? '[]' : '');?>" value="<?php echo $Option->Value?>" /></label>
+					<label><?php echo $Option->Name; ?> <input <? echo ($OptionGroup->Required ? 'class="required"' : '') ?> type="<? echo strtolower($OptionGroup->Type) ?>" <?php echo $Option->Default ? "checked='checked'" : '' ?> name="<?php echo $OptionGroup->Name; ?><?php echo ($OptionGroup->Type=='CHECKBOX' ? '[]' : '');?>" value="<?php echo $Option->Value?>" /></label>
 <?php
 		}
 	}  ?>
