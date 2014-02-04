8de117491bf662fef468b69a898b81500565fa4e
Marius Orcsik <marius.orcsik@avangate.com>
2014-02-04 18:17:25 +0200

    some ternary operator removal

--- templates/product-options.tpl.php
+++ templates/product-options.tpl.php
@@ -8,7 +8,7 @@ foreach ($prod->PriceOptions as $iKey => $OptionGroup) { ?>
 <?php
 	if ($OptionGroup->Type == 'COMBO') { 
 	?>
-					<select name="<?php echo htmlentities( $OptionGroup->Name); ?>" <? echo ($OptionGroup->Required ? 'class="required"' : ''); ?> >
+					<select name="<?php echo htmlentities( $OptionGroup->Name); ?>" <? if ($OptionGroup->Required) { echo 'class="required"'; } ?> >
 <?php 
 	if (!$OptionGroup->Required) { ?>
 
@@ -17,12 +17,12 @@ foreach ($prod->PriceOptions as $iKey => $OptionGroup) { ?>
 	}
 		/* @var $Option mPriceOptionOption */
 		foreach ($OptionGroup->Options as $iOptionKey => $Option) { ?>
-						<option value="<?php echo htmlentities( $Option->Value); ?>" <?php echo ($Option->Default ? "selected='selected'" : ''); ?>><?php echo htmlentities( $Option->Name); ?> </option>
+						<option value="<?php echo htmlentities( $Option->Value); ?>" <?php if ($Option->Default) { echo 'selected="selected"'; } ?> ><?php echo htmlentities( $Option->Name); ?> </option>
 <?php		}  ?>
 					</select>
 		
 <?php	} elseif ($OptionGroup->Type == 'INTERVAL') { ?>
-					<label><?php echo htmlentities( $OptionGroup->Name); ?> <input <? echo ($OptionGroup->Required ? 'class="required"' : ''); ?> type="text" name="<?php echo htmlentities( $OptionGroup->Name); ?>" /></label>
+					<label><?php echo htmlentities( $OptionGroup->Name); ?> <input <? if ($OptionGroup->Required) { echo 'class="required"'; } ?> type="text" name="<?php echo htmlentities( $OptionGroup->Name); ?>" /></label>
 <?php 
 	} else {
 		if (!$OptionGroup->Required && $OptionGroup->Type == 'RADIO') {
@@ -31,7 +31,7 @@ foreach ($prod->PriceOptions as $iKey => $OptionGroup) { ?>
 <?php 
 		}
 		foreach ($OptionGroup->Options as $iOptionKey => $Option) { ?>
-					<label><?php echo htmlentities( $Option->Name); ?> <input <? echo ($OptionGroup->Required ? 'class="required"' : ''); ?> type="<? echo htmlentities( strtolower($OptionGroup->Type)); ?>" <?php echo ( $Option->Default ? "checked='checked'" : ''); ?> name="<?php echo htmlentities( $OptionGroup->Name); ?><?php echo ($OptionGroup->Type=='CHECKBOX' ? '[]' : '');?>" value="<?php echo htmlentities( $Option->Value);?>" /></label>
+					<label><?php echo htmlentities( $Option->Name); ?> <input <? if ($OptionGroup->Required) { echo 'class="required"'; } ?> type="<? echo htmlentities( strtolower($OptionGroup->Type)); ?>" <?php if ( $Option->Default ) { echo 'checked="checked"'; } ?> name="<?php echo htmlentities( $OptionGroup->Name); ?><?php if($OptionGroup->Type=='CHECKBOX') { echo '[]'; }?>" value="<?php echo htmlentities( $Option->Value);?>" /></label>
 <?php
 		}
 	}  ?>
