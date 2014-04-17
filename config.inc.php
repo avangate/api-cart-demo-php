2632e2ed196c623eee7e64e5b8aeed28ce05ad5b
Marius Orcsik <marius.orcsik@avangate.com>
2014-04-17 15:26:47 +0300

    Added a token config file

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+define ('BASE_URL', 'https://sandbox.avangate.com/api/order/2.0/');
+define ('PAYMENT_URL', BASE_URL . 'setpaymentdetails.php');
+
+define ('ORDER_SOAP_URL', BASE_URL . 'soap/');
+
+
+define ('MCODE', 'MERCHANT_CODE');
+define ('KEY', 'SECRET');
+
+define ('USE_PROXY', false);
