09de535be089a3f81db6170bde9c3de6cbd5bbd9
Marius Orcsik <marius.orcsik@avangate.com>
2014-04-17 15:24:23 +0300

    Added search options for list-products

--- list-products.php
+++ list-products.php
@@ -3,10 +3,14 @@
 //$SearchOptions['status'] = true;
 $SearchOptions['NetworkCrosselling'] = true;
 $SearchOptions['type'] = 'REGULAR';
+$SearchOptions['page'] = array(
+	'page_size' => 1,
+	'page_number' => 2
+);
 try {
 	$a = array();
 
-//	var_dump ($c);die;
+//	var_dump ($c, $SearchOptions);die;
 	$a = $c->searchProducts($SearchOptions);
 } catch (SoapFault $e) {
 	if ($e->getMessage() == 'Invalid hash provided') {
